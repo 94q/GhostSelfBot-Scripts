3e6e00e42afd76d66d63655c96634a973e4c43e2
ice bear <80714938+5mf@users.noreply.github.com>
2021-12-12 19:16:12 +0000

    new scripts , hope u like it! (READ FAQ FOR INFO)

---
+++ primordialdev.py
@@ -0,0 +1,7 @@
+@BetterGhost.command()
+async def generateinviteprimordial(ctx):
+    def id_generator(size=40, chars=string.ascii_lowercase[0:6] + string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
+    await ctx.message.delete()
+    avatar_embed = discord.Embed(color = discord.Colour(0xFF0000),title = f'Enjoy Primordial.Dev!!', description=f'Here is your invite {ctx.message.author.mention} : {id_generator()}')
+    await ctx.send(embed=avatar_embed)
